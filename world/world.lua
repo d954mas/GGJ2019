8677e37903092ab2a38fd782f00fb8c56fe12096
d954mas <d954mas@mail.ru>
2019-01-24 23:54:57 +0300

    add  ecs

--- world/world.lua
+++ world/world.lua
@@ -1,6 +1,7 @@
 local COMMON = require "libs.common"
 local Observable = require "libs.observable_mixin"
 local MultipleSubscription = require "libs.multiple_subscription"
+local ECS = require "libs.ecs"
 
 ---@class World:Observable
 local M = COMMON.class("World")
@@ -8,11 +9,19 @@ M:include(Observable)
 local EVENTS = {
 }
 
+---@param self World
+local function init_systems(self)
+	self.ecs_world
+end
+
 function M:initialize()
 	self.EVENTS = EVENTS
 	self.subscription = MultipleSubscription()
 	self:set_observable_events(self.EVENTS)
+	self.ecs_world = ECS.world()
 end
+
+function M
 
 function M:update(dt, no_save)
 end
@@ -23,6 +32,7 @@ function M:save(file)
 end
 
 function M:dispose()
+	self.ecs_world:clear()
 end
 
 function M:load(file)
