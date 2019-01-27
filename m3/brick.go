6ea6ef39d49923747ab95d152bf71b27d50f8170
d954mas <d954mas@mail.ru>
2019-01-27 10:21:14 +0300

    m3

---
+++ m3/brick.go
@@ -0,0 +1,35 @@
+components {
+  id: "brick"
+  component: "/m3/brick.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/examples/basic/basic.atlas\"\n"
+  "default_animation: \"brickBlue06\"\n"
+  "material: \"/assets/materials/sprite_m3.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
